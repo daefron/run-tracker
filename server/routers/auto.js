ae81d9a1797ab1b6655b00f2939d25354280fdf2
Daefron <thomas_evans@outlook.com>
2025-01-24 12:54:16 +1100

    fixed auth/update not auto firing

--- server/routers/auto.js
+++ server/routers/auto.js
@@ -3,32 +3,24 @@ const fitbitDataController = require("../controllers/fitbitDataController");
 
 function timers() {
   //time between run updates (30 mins)
-  // const runFetchDelay = 1800000;
-  // const runFetchFunc = () => fitbitDataController.updateGet();
-  // autoTimer(runFetchDelay, runFetchFunc());
-  // console.log(
-  //   "Run auto-updater running. Interval = " + runFetchDelay / 60000 + " mins"
-  // );
-
-  // //time between auth refresh updates (7.5 hours as tokens last 8 hours)
-  // const refreshAuthDelay = 27000000;
-  // const refreshAuthFunc = () => authController.refreshAuth();
-  // autoTimer(refreshAuthDelay, refreshAuthFunc());
-  // console.log(
-  //   "Auth refresh auto-updater running. Interval = " +
-  //     refreshAuthDelay / 60000 +
-  //     " mins"
-  // );
-
+  const runFetchDelay = 1800000;
+  console.log(
+    "Run auto-updater running. Interval = " + runFetchDelay / 60000 + " mins."
+  );
   setInterval(() => {
-    console.log("ping");
-  }, 5000);
+    fitbitDataController.updateGet();
+  }, runFetchDelay);
 
-  function autoTimer(delay, func) {
-    setInterval(() => {
-      func;
-    }, delay);
-  }
+  //time between auth refresh updates (7.5 hours as tokens last 8 hours)
+  const refreshAuthDelay = 27000000;
+  console.log(
+    "Auth refresh auto-updater running. Interval = " +
+      refreshAuthDelay / 60000 +
+      " mins."
+  );
+  setInterval(() => {
+    authController.refreshAuth();
+  }, refreshAuthDelay);
 }
 
 module.exports = {
